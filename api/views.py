02aff62c988bd6e57b5903db8c411630d0aa2676
Eswarrajpurohit <eswaritsme@gmail.com>
2020-05-12 14:23:37 +0000

    debugging json api for token

--- api/views.py
+++ api/views.py
@@ -12,7 +12,8 @@ from rest_framework.status import (
     HTTP_200_OK
 )
 from django.contrib.auth import authenticate
-
+from rest_framework.parsers import JSONParser,FormParser
+from rest_framework.decorators import parser_classes
 
 # Create your views here.
 
@@ -27,11 +28,19 @@ def api(request):
 @csrf_exempt
 @api_view(["POST"])
 @permission_classes((AllowAny,))
-def apilogin(request):
-    username = request.data.get("username")
-    password = request.data.get("password")
+@parser_classes([JSONParser,FormParser])
+def apilogin(request,format=None):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    content=request.data.get("_content")
     if username is None or password is None:
-        return Response({'error': 'Please provide both username and password'},
+        return Response({'error': 'Please provide both username and password',
+            "username":username,
+            "password":password,
+            "reaq":request.data,
+            "reaqu":request.data.get("_content"),
+            "type":str(type(content))
+            },
                         status=HTTP_400_BAD_REQUEST)
     user = authenticate(username=username, password=password)
     if not user:
@@ -40,3 +49,6 @@ def apilogin(request):
     token, _ = Token.objects.get_or_create(user=user)
     return Response({'token': token.key},
                     status=HTTP_200_OK)
+
+
+
