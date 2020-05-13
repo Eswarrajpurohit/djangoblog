9ac10bd285387fc535c78092abf116fdd53d0a97
Eswarrajpurohit <eswaritsme@gmail.com>
2020-05-13 12:07:17 +0530

    configuring for static files

--- myblog/settings.py
+++ myblog/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'article/static')
+    os.path.join(BASE_DIR,'article/static/article')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
